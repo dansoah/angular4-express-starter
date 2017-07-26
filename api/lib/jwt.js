039ec873130b9ab910e399a0e1867e1dec72e55d
Danilo Rechi <danilo.rechi@gmail.com>
2017-07-26 00:36:18 -0300

    add jwt lib

---
+++ api/lib/jwt.js
@@ -0,0 +1,50 @@
+import jwt from 'jsonwebtoken'
+
+const secret = process.env.JWT_SECRET;
+
+/**
+ * Validates if a token is a valid JWT token
+ * @param {String} token - token to be verified
+ * @returns {Promise}
+ */
+export function verifyToken(token) {
+    return new Promise( (resolve, reject) => {
+        jwt.verify( token,
+                    secret,
+                    (err, decodedToken) =>  err || !decodedToken
+                                                        ? return reject(err)
+                                                        : resolve(decodedToken)
+                  );
+    });
+}
+
+export function createToken(details) {
+
+    details = !!details ? details : {};
+    details.maxAge = !details.maxAge || isNaN(parseInt(detais.maxAge)) ?
+                        details.maxAge = 3600 : details.axAge;
+    details.sessionData = Object.keys( details.sessionData || {} )
+                                .reduce( (obj, key) => {
+                                    if(key === 'password')
+                                        return obj;
+
+                                    if(typeof details.sessionData === 'function')
+                                        return obj;
+
+                                    Object.assign(obj,details.sessionData[key]);
+                                })
+
+    const token = jwt.sign({ data: details.sessionData },
+                             secret,
+                             expiresIn: details.maxAge,
+                             algorithm: 'HS256'
+                         );
+
+    return token;
+
+}
+
+export default {
+  verifyToken,
+  createToken
+}
