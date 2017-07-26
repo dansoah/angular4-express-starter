245257d285e040f90064f30fb2b52a115010ac4a
Danilo Rechi <danilo.rechi@gmail.com>
2017-07-26 01:25:16 -0300

    add dummy athentication route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import jwtValidate from '../middlewares/jwt-validate';
+import { createToken } from '../lib/jwt';
+import { authenticate } from '../services/authentication';
 
 const router = express.Router();
 
@@ -8,7 +10,25 @@ router.get('/', (req,res) => {
 })
 
 router.get('/teste', jwtValidate, (req,res) => {
-    res.send({isRunning:true});
+    res.send({isTesting:true});
+})
+
+router.post('/authenticate', (req,res) => {
+    let { email, password } = req.body;
+    const closeTheDoor = res
+                            .status(401)
+                            .send
+                            .bind(res, {
+                                status: 401,
+                                message: 'Wrong email/password'
+                            });
+
+    authenticate(email,password).then( userInfo => {
+        let token = createToken({sessionData: userInfo});
+        res.send({status:200, data:token})
+    }, () => {
+        closeTheDoor()
+    })
 })
 
 export default router;
