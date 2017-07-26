3c8ad9214cf64c3ca596330e330d0e267c402acd
Danilo Rechi <danilo.rechi@gmail.com>
2017-07-26 01:00:38 -0300

    add dummy route to test jwt validation

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import jwtValidate from '../middlewares/jwt-validate';
+
 const router = express.Router();
 
 router.get('/', (req,res) => {
+    res.send({isRunning:true});
+})
+
+router.get('/teste', jwtValidate, (req,res) => {
     res.send({isRunning:true});
 })
 
